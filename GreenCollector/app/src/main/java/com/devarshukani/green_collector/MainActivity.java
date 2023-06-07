0f1b20bde35a1b8bfbe7bb83e6e9288a16372fd5
Devarsh Ukani <me.devarshukani@gmail.com>
2023-06-08 00:18:07 +0530

    google map progress
    - added custom markers for source and destination
    - added proper routing and polyline
    - users marker updates with its current location
    - added 2 dialog boxes and 1 bottom sheet dialog for the UI

--- GreenCollector/app/src/main/java/com/devarshukani/green_collector/MainActivity.java
+++ GreenCollector/app/src/main/java/com/devarshukani/green_collector/MainActivity.java
@@ -1,13 +1,17 @@
 package com.devarshukani.green_collector;
 
-import static android.graphics.Color.GREEN;
-
 import android.Manifest;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,9 +27,16 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.json.JSONArray;
@@ -53,6 +64,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private Polyline routePolyline; // Polyline for the route
     private List<LatLng> routePoints; // List of points for the route
     private LatLng currentLocation;
+    private Marker currentLocationMarker;
+    private BitmapDescriptor customMarkerIcon;
+    private boolean startlater = false;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +88,22 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
+        fabReachedDestination.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(startlater == false){
+                    showCustomDialogReachedDestination();
+                }
+                else{
+                    startlater = false;
+                    startRoute();
+
+                }
+            }
+        });
+
+        showCustomDialogStartGarbageCollection();
+
         mapView = findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
@@ -81,24 +113,176 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         fabRecenter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                recenterMap();
-                // Set the destination and source coordinates (dummy values for demonstration)
-                destination = new LatLng(37.7749, -122.4194); // San Francisco
-                source = new LatLng(37.7749, -122.4312); // Source location
-
-                // Call Directions API to get the optimized route and draw the polyline
-                getDirections(source, destination);
+                recenterMap();
             }
         });
+
     }
+
+    private void showCustomDialogStartGarbageCollection() {
+        // Create a dialog object
+        Dialog dialog = new Dialog(this, R.style.CustomDialog);
+
+        dialog.setContentView(R.layout.dialog_start_garbage_collection);
+        dialog.setCanceledOnTouchOutside(false);
+
+        Button startButton = dialog.findViewById(R.id.startButton);
+        TextView startLaterLink = dialog.findViewById(R.id.startLaterLink);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRoute();
+                dialog.dismiss();
+            }
+        });
+        startLaterLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startlater = true;
+                dialog.dismiss();
+            }
+        });
+
+        // Show the dialog
+        dialog.show();
+    }
+
+    private void showCustomDialogReachedDestination() {
+        // Create a dialog object
+        Dialog dialog = new Dialog(this, R.style.CustomDialog);
+
+        dialog.setContentView(R.layout.dialog_reached_destination);
+        dialog.setCanceledOnTouchOutside(false);
+
+        Button scanQRCodeButton = dialog.findViewById(R.id.scanQRCodeButton);
+        TextView continueWithoutScanningLink = dialog.findViewById(R.id.continueWithoutScanningLink);
+        scanQRCodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        continueWithoutScanningLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCustomDialogStartNextDestination();
+                dialog.dismiss();
+            }
+        });
+
+        // Show the dialog
+        dialog.show();
+    }
+
+    private void showCustomDialogStartNextDestination() {
+        // Create a dialog object
+        BottomSheetDialog dialog = new BottomSheetDialog(this);
+
+        dialog.setContentView(R.layout.dialog_start_next_destination);
+        dialog.setCanceledOnTouchOutside(false);
+
+        Button continueCollectingButton = dialog.findViewById(R.id.continueCollectingButton);
+        Button dumpGarbageButton = dialog.findViewById(R.id.dumpGarbageButton);
+        TextView stopCollectingLink = dialog.findViewById(R.id.stopCollectingLink);
+        continueCollectingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleMap.clear();
+                startRoute();
+                dialog.dismiss();
+            }
+        });
+
+        dumpGarbageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        stopCollectingLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        // Show the dialog
+        dialog.show();
+    }
+
+
+
+
+    public void startRoute() {
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            // Request the user's current location
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        // Location found, retrieve latitude and longitude
+                        double latitude = location.getLatitude();
+                        double longitude = location.getLongitude();
+
+                        // Do something with the retrieved location coordinates
+                        Log.d("Location", "Latitude: " + latitude + " Longitude: " + longitude);
+                        source = new LatLng(latitude, longitude);
+                        destination = new LatLng(22.2770064, 70.7828704);
+
+                        BitmapDescriptor customMarkerIcon;
+//                        customMarkerIcon = BitmapDescriptorFactory.fromResource(R.drawable.garbage_van_marker);
+//                        MarkerOptions markerOptionsSource = new MarkerOptions()
+//                                .position(source)
+//                                .title("You")
+//                                .icon(customMarkerIcon)
+//                                .anchor(0.5f, 0.5f);
+//                        sourceMarker = googleMap.addMarker(markerOptionsSource);
+
+
+                        customMarkerIcon = BitmapDescriptorFactory.fromResource(R.drawable.dustbin_marker);
+                        MarkerOptions markerOptionsDestination = new MarkerOptions()
+                                .position(destination)
+                                .title("Collection Point")
+                                .icon(customMarkerIcon)
+                                .anchor(0.5f,0.5f);
+                        googleMap.addMarker(markerOptionsDestination);
+
+                        getDirections(source, destination);
+                    } else {
+                        // Location is null, handle the case when the location is not available
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this, "Failed to access location", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            // Request permission to access the user's location
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+
+//        source = new LatLng(22.2781274, 70.7743364);
+//        destination = new LatLng(22.2770064, 70.7828704);
+//
+//        // Call Directions API to get the optimized route and draw the polyline
+//        getDirections(source, destination);
+    }
+
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         this.googleMap = googleMap;
         // Check for location permission
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            googleMap.setMyLocationEnabled(true);
-            googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+            customMarkerIcon = BitmapDescriptorFactory.fromResource(R.drawable.garbage_van_marker);
+            googleMap.setMyLocationEnabled(false);
+            googleMap.getUiSettings().setMyLocationButtonEnabled(true);
             setupLocationUpdates();
         } else {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
@@ -249,20 +433,32 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         if (routePoints != null && routePoints.size() > 0) {
             PolylineOptions polylineOptions = new PolylineOptions()
                     .addAll(routePoints)
-                    .width(10)
-                    .color(GREEN);
+                    .width(12)
+                    .color(Color.rgb(76,175,80));
 
             routePolyline = googleMap.addPolyline(polylineOptions);
 
             // Move the camera to the destination
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(destination, 18f));
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(source, 18f));
         }
     }
+
+
 
     private void updateCurrentLocationMarker(LatLng location) {
         // Update the marker for current location on the map
         // Remove previous marker if exists
         // Add new marker for current location
+        if (currentLocationMarker != null) {
+            currentLocationMarker.remove();
+        }
+        // Add new marker for current location
+        MarkerOptions markerOptions = new MarkerOptions()
+                .position(location)
+                .title("Current Location")
+                .icon(customMarkerIcon)
+                .anchor(0.5f, 0.5f);
+        currentLocationMarker = googleMap.addMarker(markerOptions);
     }
 
     private void recenterMap() {
@@ -311,6 +507,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapView.onLowMemory();
     }
 
+
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -325,7 +523,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 // for ActivityCompat#requestPermissions for more details.
                 return;
             }
-            googleMap.setMyLocationEnabled(true);
+            googleMap.setMyLocationEnabled(false);
             googleMap.getUiSettings().setMyLocationButtonEnabled(false);
             setupLocationUpdates();
         }
