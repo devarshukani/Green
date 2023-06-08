e7a5725fea45e9f719b86b899883c1d7b679169b
Devarsh Ukani <me.devarshukani@gmail.com>
2023-06-08 22:32:34 +0530

    display QR Code, schedule pickup bottom sheet

--- Green-User/app/src/main/java/com/devarshukani/green_user/fragment/HomeFragment.java
+++ Green-User/app/src/main/java/com/devarshukani/green_user/fragment/HomeFragment.java
@@ -1,7 +1,10 @@
 package com.devarshukani.green_user.fragment;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 
@@ -15,6 +18,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,9 +29,14 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -52,6 +62,7 @@ public class HomeFragment extends Fragment {
     FirebaseFirestore db;
     TextView helloHomeTextView;
     CardView wastePickupClick;
+    Button qrCodeButton;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
     public HomeFragment() {
@@ -95,6 +106,7 @@ public class HomeFragment extends Fragment {
         db = FirebaseFirestore.getInstance();
         helloHomeTextView = view.findViewById(R.id.helloHomeTextView);
         wastePickupClick = view.findViewById(R.id.wastePickupClick);
+        qrCodeButton = view.findViewById(R.id.qrCodeButton);
 
         db.collection("users").document(auth.getCurrentUser().getUid())
                 .get()
@@ -121,53 +133,151 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View v) {
 
-                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-
-                    FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
-
-                    fusedLocationClient.getLastLocation()
-                            .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
-                                @Override
-                                public void onSuccess(Location location) {
-                                    if (location != null) {
-                                        // Get the latitude and longitude
-                                        double latitude = location.getLatitude();
-                                        double longitude = location.getLongitude();
-
-                                        Map<String, Object> userlocationdata = new HashMap<>();
-                                        userlocationdata.put("latitude", latitude);
-                                        userlocationdata.put("longitude", longitude);
-
-                                        db.collection("userlocation").document(auth.getCurrentUser().getUid())
-                                                .set(userlocationdata)
-                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                    @Override
-                                                    public void onSuccess(Void aVoid) {
-                                                        // Location data stored successfully
-                                                        Toast.makeText(getContext(), "Location Saved", Toast.LENGTH_SHORT).show();
-                                                    }
-                                                })
-                                                .addOnFailureListener(new OnFailureListener() {
-                                                    @Override
-                                                    public void onFailure(@NonNull Exception e) {
-                                                        // Failed to store location data
-                                                        Toast.makeText(getContext(), e.toString(), Toast.LENGTH_SHORT).show();
-                                                        Log.d("ERROR",e.toString());
-                                                    }
-                                                });
-                                    }
-                                }
-                            });
-
-                } else {
-                    // Permission not granted, request it from the user
-                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
-                }
-
-
+                showCustomDialogWastePickupQuestions();
             }
         });
 
+        qrCodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCustomDialogQRCodeImage();
+            }
+        });
+
+
+
         return view;
+    }
+
+    private void showCustomDialogWastePickupQuestions() {
+        // Create a dialog object
+        BottomSheetDialog dialog = new BottomSheetDialog(getContext());
+
+        dialog.setContentView(R.layout.dialog_waste_pickup_questions);
+
+        Button yesButton = dialog.findViewById(R.id.yesButton);
+        Button partialButton = dialog.findViewById(R.id.partialButton);
+        Button noButton = dialog.findViewById(R.id.noButton);
+        TextView notSureLink = dialog.findViewById(R.id.notSureLink);
+
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUserData("yes");
+                dialog.dismiss();
+            }
+        });
+        partialButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUserData("partial");
+                dialog.dismiss();
+            }
+        });
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUserData("no");
+                dialog.dismiss();
+            }
+        });
+        notSureLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUserData("notsure");
+                dialog.dismiss();
+            }
+        });
+
+        // Show the dialog
+        dialog.show();
+    }
+
+    private void updateUserData(String answer){
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            if (location != null) {
+                                // Get the latitude and longitude
+                                double latitude = location.getLatitude();
+                                double longitude = location.getLongitude();
+
+                                Map<String, Object> userlocationdata = new HashMap<>();
+                                userlocationdata.put("latitude", latitude);
+                                userlocationdata.put("longitude", longitude);
+                                userlocationdata.put("isrecyclable", answer);
+
+                                db.collection("users").document(auth.getCurrentUser().getUid())
+                                        .update(userlocationdata)
+                                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                // Location data stored successfully
+                                                Toast.makeText(getContext(), "Your request has been saved", Toast.LENGTH_SHORT).show();
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                // Failed to store location data
+                                                Toast.makeText(getContext(), e.toString(), Toast.LENGTH_SHORT).show();
+                                                Log.d("ERROR",e.toString());
+                                            }
+                                        });
+                            }
+                        }
+                    });
+
+        } else {
+            // Permission not granted, request it from the user
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private void showCustomDialogQRCodeImage() {
+        // Create a dialog object
+        Dialog dialog = new Dialog(getContext());
+
+        dialog.setContentView(R.layout.dialog_show_qr_code);
+
+        ImageView qrCodeImageView = dialog.findViewById(R.id.qrCodeImageView);
+
+        String data = auth.getCurrentUser().getUid();
+
+        try {
+            // Generate a QR code with the given data
+            Bitmap qrCodeBitmap = generateQRCode(data, 700, 700);
+
+            // Set the generated QR code bitmap to the ImageView
+            qrCodeImageView.setImageBitmap(qrCodeBitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
+        // Show the dialog
+        dialog.show();
+    }
+
+    private Bitmap generateQRCode(String data, int width, int height) throws WriterException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, width, height);
+
+        int[] pixels = new int[width * height];
+        for (int y = 0; y < height; y++) {
+            int offset = y * width;
+            for (int x = 0; x < width; x++) {
+                pixels[offset + x] = bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE;
+            }
+        }
+
+        Bitmap qrCodeBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        qrCodeBitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+
+        return qrCodeBitmap;
     }
 }
